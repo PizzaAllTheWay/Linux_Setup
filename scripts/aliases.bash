413888753ed6819b4f460470cdc93520a64d5b46
Martynas Smilingis <smilingis.martynas@gmail.com>
2026-01-05 18:44:12 +0100

    remade simpler

--- scripts/aliases.bash
+++ scripts/aliases.bash
@@ -53,7 +53,7 @@ ssh() {
 # RDP (Remote Desktop Protocol) to work PC
 # NOTE: Requires Nordic internal VPN for this to work (Global Protect VPN)
 rdpnordic() {
-  RDP_USER="martynas.smilingis@nordicsemi.no"
+  RDP_USER="masm"
   RDP_DOMAIN="NVLSI"
   RDP_HOST="10.250.15.112" # Internal IP of my machine after connecting to VPN (Global Protect VPN)
   rdesktop -u "$RDP_USER" -d "$RDP_DOMAIN" -g 90% -k no -p - "$RDP_HOST"
